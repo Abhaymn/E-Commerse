2a29bb16fb6589d461b2e5a8f697e793889ff7ca
abhaymn <abhaykrishnatsy007>
2023-01-02 11:55:58 +0530

    products

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
+  React Bootstrap Configuration
+*/
+
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
